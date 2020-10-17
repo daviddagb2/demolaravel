de98ec3a26ddbed7dd3bacf59b63afde6dc272a6
DavidGB <daviddagb2@gmail.com>
2020-10-17 16:06:33 -0600

    actualice el home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -52,6 +52,8 @@
                 BIENVENIDO AL SITIO LARAVEL DE PRUEBA
 
                 <b>esto ha sido editado </b>
+                <br/>
+                <b>Este welcome ha sido editado por develop 1 </b>
 
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
