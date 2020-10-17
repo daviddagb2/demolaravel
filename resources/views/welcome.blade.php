acf4b8e9c6c0f1ebc60498414ee7352f53a9912d
DavidGB <daviddagb2@gmail.com>
2020-10-17 15:52:52 -0600

    editado un bold

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -51,6 +51,8 @@
 
                 BIENVENIDO AL SITIO LARAVEL DE PRUEBA
 
+                <b>esto ha sido editado </b>
+
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
                         <div class="p-6">
